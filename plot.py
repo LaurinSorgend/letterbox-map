865e5b303e5cea52ad8cf94876d0670552a74c5e
Laurin Sorgend <32600511+LaurinSorgend@users.noreply.github.com>
2024-09-20 19:04:11 +0200

    Update plot.py

--- plot.py
+++ plot.py
@@ -3,11 +3,9 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Load the movie data from the JSON file
 with open('movies_with_countries.json', 'r', encoding='utf-8') as json_file:
     movies = json.load(json_file)
 
-# Flatten the data to create a DataFrame
 data = []
 for movie in movies:
     for country in movie['origin_country']:
@@ -18,18 +16,15 @@ for movie in movies:
 
 df = pd.DataFrame(data)
 
-# Calculate the average rating and movie count per country
 avg_rating = df.groupby('country')['rating'].mean().reset_index()
 movie_count = df.groupby('country')['rating'].size().reset_index(name='count')
 
-# Load the world map shapefile
 world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
 
-# Merge the world map with the average rating data
 world_avg_rating = world.merge(avg_rating, how='left', left_on='name', right_on='country')
 world_movie_count = world.merge(movie_count, how='left', left_on='name', right_on='country')
 
-# Plot the average rating per country
+
 fig, ax = plt.subplots(1, 2, figsize=(20, 10))
 
 world_avg_rating.plot(column='rating', ax=ax[0], legend=True,
@@ -39,7 +34,6 @@ world_avg_rating.plot(column='rating', ax=ax[0], legend=True,
 ax[0].set_title('Average Rating by Country')
 ax[0].set_axis_off()
 
-# Plot the movie count per country
 world_movie_count.plot(column='count', ax=ax[1], legend=True,
                        legend_kwds={'label': "Movie Count by Country",
                                     'orientation': "horizontal"},
@@ -47,10 +41,7 @@ world_movie_count.plot(column='count', ax=ax[1], legend=True,
 ax[1].set_title('Movie Count by Country')
 ax[1].set_axis_off()
 
-# Save the plots as SVG files
-fig.savefig('average_rating_by_country.svg', format='svg')
-fig.savefig('movie_count_by_country.svg', format='svg')
+fig.savefig('plot.svg', format='svg')
 
-# Show the plots
 plt.tight_layout()
 plt.show()
